ec69c5eac549caf9dc5510e3d61628975b93e979
SoftPoachedEggs <115499632+SoftPoachedEggs@users.noreply.github.com>
2023-04-27 18:40:55 -0400

    debugging install

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,14 +1,17 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+// Import Workbox modules
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
+// Pre-cache all files in the __WB_MANIFEST array
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Create a CacheFirst strategy for page caching
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
@@ -19,27 +22,58 @@ const pageCache = new CacheFirst({
   ],
 });
 
+// // Define a matchCallback function to determine which requests to cache as static resources
+// const cacheName = "static-resources";
+// const matchCallback = ({ request }) => {
+//   console.log(request);
+//   return request.destination === "style" || request.destination === "script";
+// };
+
+// Warm the cache by pre-caching specified URLs with the pageCache strategy
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ["/index.html", "/"],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+//Register a route to cache all requests that match the matchCallback function
+registerRoute(
+  matchCallback,
+  new StaleWhileRevalidate({
+    cacheName,
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+// Register a route to cache all navigation requests with the pageCache strategy
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // TODO: Implement asset caching
-const assetCache = new CacheFirst({
-  cacheName: 'asset-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 7 * 24 * 60 * 60,
-    }),
-  ],
-});
+// Create a CacheFirst strategy for asset caching
+// const assetCache = new CacheFirst({
+// cacheName: 'asset-cache',
+// plugins: [
+// new CacheableResponsePlugin({
+// statuses: [0, 200],
+// }),
+// new ExpirationPlugin({
+// maxAgeSeconds: 7 * 24 * 60 * 60,
+// }),
+// ],
+// });
 
-registerRoute(
-  /\.(?:js|css|png|gif|jpg|jpeg|svg)$/,
-  assetCache
-);
+// Register a route to cache all requests for static assets
+// registerRoute(
+//   ({ request }) => ["style", "script", "worker"].includes(request.destination),
+//   new StaleWhileRevalidate({
+//     cacheName: "asset-cache",
+//     plugins: [
+//       new CacheableResponsePlugin({
+//         statuses: [0, 200],
+//       }),
+//     ],
+//   })
+// );
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,56 +1,70 @@
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+//plugin that generates an HTML file to serve the webpack bundles
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+//plugin that generates a web app manifest file that provides metadata about the application. 
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+//Node.js module used to resolve file paths.
+const path = require("path");
+//plugin that generates a service worker file that will cache and serve the application's assets.
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    //specifies the mode in which webpack should run
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      //specifies the entry point(s) for the application
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
+    //specifies where the output files should be saved
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
+    // Specifies the plugins that will be used
     plugins: [
+       // Generates an HTML file to serve the webpack bundles
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'JATE'
+        template: "./index.html",
+        title: "JATE",
       }),
+       // Generates a service worker file that will cache and serve the application's assets
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      // Generates a web app manifest file that provides metadata about the application
       new WebpackPwaManifest({
-        name: 'Just Another Text Editor',
+        name: "Just Another Text Editor",
+        fingerprints: false,
         inject: true,
-        short_name: 'J.A.T.E',
-        description: 'This is my PWA',
-        background_color: '#ffffff',
-        theme_color: '#007bff',
+        short_name: "JATE",
+        description: "This is my PWA",
+        background_color: "#ffffff",
+        theme_color: "#007bff",
+        start_url: "/",
+        publicPath: "/",
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('icons'),
+            destination: path.join("assets", "icons"),
           },
         ],
-        start_url: '/',
-        publicPath: '/'
-      }),
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'sw-sw.js',
       }),
     ],
-
+    // Specifies how webpack should handle different types of files
     module: {
       rules: [
+        // Handles CSS files using the "style-loader" and "css-loader" plugins
         {
           test: /\.css$/i,
           use: ["style-loader", "css-loader"],
         },
+        // Handles JavaScript files using the "babel-loader" plugin with some additional plugins
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
